7332e101b8d33022eb5b323178be0044d4edd6f1
Paddy Morgan <patrickmorgan1984@gmail.com>
2022-03-08 22:35:17 +0000

    Get full coverage on points command

--- cmd/points_test.go
+++ cmd/points_test.go
@@ -1,12 +1,14 @@
 package cmd
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/golang/mock/gomock"
 
 	"github.com/paddymorgan84/fpl/api"
 	"github.com/paddymorgan84/fpl/helpers"
+	"github.com/paddymorgan84/fpl/responses"
 	"github.com/paddymorgan84/fpl/ui"
 )
 
@@ -30,5 +32,207 @@ func TestBuildPointsCommand(t *testing.T) {
 	var expectedUse = "points"
 	if cmd.Use != expectedUse {
 		t.Fatalf("expected: %v; got %v", expectedUse, cmd.Use)
+	}
+}
+
+func TestWhenTeamIDRetrievalErrorsItsHandledCorrectly(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	reader := helpers.NewMockConfigReader(ctrl)
+	fpl := api.NewMockFplAPI(ctrl)
+	teamsParser := helpers.NewMockTeamsParser(ctrl)
+	renderer := ui.NewMockRenderer(ctrl)
+	gameweekParser := helpers.NewMockGameweekParser(ctrl)
+	expectedError := "Whoops"
+
+	teamsParser.EXPECT().
+		GetTeamID(gomock.Any(), gomock.Any()).
+		Return(-1, errors.New(expectedError))
+
+	cmd := BuildPointsCommand(fpl, reader, teamsParser, gameweekParser, renderer)
+
+	err := cmd.RunE(cmd, make([]string, 0))
+
+	if err.Error() != expectedError {
+		t.Fatalf("expected: %v; got %v", expectedError, err)
+	}
+}
+
+func TestWhenBootstrapDataRetrievalErrorsItsHandledCorrectly_Points(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	reader := helpers.NewMockConfigReader(ctrl)
+	fpl := api.NewMockFplAPI(ctrl)
+	teamsParser := helpers.NewMockTeamsParser(ctrl)
+	renderer := ui.NewMockRenderer(ctrl)
+	gameweekParser := helpers.NewMockGameweekParser(ctrl)
+	expectedError := "Whoops"
+
+	teamsParser.EXPECT().
+		GetTeamID(gomock.Any(), gomock.Any()).
+		Return(1, nil)
+
+	fpl.EXPECT().
+		GetBootstrapData().
+		Return(responses.BootstrapData{}, errors.New(expectedError))
+
+	cmd := BuildPointsCommand(fpl, reader, teamsParser, gameweekParser, renderer)
+
+	err := cmd.RunE(cmd, make([]string, 0))
+
+	if err.Error() != expectedError {
+		t.Fatalf("expected: %v; got %v", expectedError, err)
+	}
+}
+
+func TestWhenCurrentGameweekRetrievalErrorsItsHandledCorrectly_Points(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	reader := helpers.NewMockConfigReader(ctrl)
+	fpl := api.NewMockFplAPI(ctrl)
+	teamsParser := helpers.NewMockTeamsParser(ctrl)
+	renderer := ui.NewMockRenderer(ctrl)
+	gameweekParser := helpers.NewMockGameweekParser(ctrl)
+	expectedError := "Whoops"
+
+	teamsParser.EXPECT().
+		GetTeamID(gomock.Any(), gomock.Any()).
+		Return(1, nil)
+
+	fpl.EXPECT().
+		GetBootstrapData().
+		Return(responses.BootstrapData{}, nil)
+
+	gameweekParser.EXPECT().
+		GetCurrentGameweek(gomock.Any(), gomock.Any()).
+		Return(-1, errors.New(expectedError))
+
+	cmd := BuildPointsCommand(fpl, reader, teamsParser, gameweekParser, renderer)
+
+	err := cmd.RunE(cmd, make([]string, 0))
+
+	if err.Error() != expectedError {
+		t.Fatalf("expected: %v; got %v", expectedError, err)
+	}
+}
+
+func TestWhenGameweekPointsRetrievalErrorsItsHandledCorrectly_Points(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	reader := helpers.NewMockConfigReader(ctrl)
+	fpl := api.NewMockFplAPI(ctrl)
+	teamsParser := helpers.NewMockTeamsParser(ctrl)
+	renderer := ui.NewMockRenderer(ctrl)
+	gameweekParser := helpers.NewMockGameweekParser(ctrl)
+	expectedError := "Whoops"
+
+	teamsParser.EXPECT().
+		GetTeamID(gomock.Any(), gomock.Any()).
+		Return(1, nil)
+
+	fpl.EXPECT().
+		GetBootstrapData().
+		Return(responses.BootstrapData{}, nil)
+
+	gameweekParser.EXPECT().
+		GetCurrentGameweek(gomock.Any(), gomock.Any()).
+		Return(1, nil)
+
+	fpl.EXPECT().
+		GetGameweekPoints(gomock.Any(), gomock.Any()).
+		Return(responses.GameweekPoints{}, errors.New(expectedError))
+
+	cmd := BuildPointsCommand(fpl, reader, teamsParser, gameweekParser, renderer)
+
+	err := cmd.RunE(cmd, make([]string, 0))
+
+	if err.Error() != expectedError {
+		t.Fatalf("expected: %v; got %v", expectedError, err)
+	}
+}
+
+func TestWhenGameweekLiveScoresRetrievalErrorsItsHandledCorrectly_Points(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	reader := helpers.NewMockConfigReader(ctrl)
+	fpl := api.NewMockFplAPI(ctrl)
+	teamsParser := helpers.NewMockTeamsParser(ctrl)
+	renderer := ui.NewMockRenderer(ctrl)
+	gameweekParser := helpers.NewMockGameweekParser(ctrl)
+	expectedError := "Whoops"
+
+	teamsParser.EXPECT().
+		GetTeamID(gomock.Any(), gomock.Any()).
+		Return(1, nil)
+
+	fpl.EXPECT().
+		GetBootstrapData().
+		Return(responses.BootstrapData{}, nil)
+
+	gameweekParser.EXPECT().
+		GetCurrentGameweek(gomock.Any(), gomock.Any()).
+		Return(1, nil)
+
+	fpl.EXPECT().
+		GetGameweekPoints(gomock.Any(), gomock.Any()).
+		Return(responses.GameweekPoints{}, nil)
+
+	fpl.EXPECT().
+		GetGameweekLiveScores(gomock.Any()).
+		Return(responses.GameweekLiveScores{}, errors.New(expectedError))
+
+	cmd := BuildPointsCommand(fpl, reader, teamsParser, gameweekParser, renderer)
+
+	err := cmd.RunE(cmd, make([]string, 0))
+
+	if err.Error() != expectedError {
+		t.Fatalf("expected: %v; got %v", expectedError, err)
+	}
+}
+
+func TestPointsReturnedSuccessfullyNoErrorReturned(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	reader := helpers.NewMockConfigReader(ctrl)
+	fpl := api.NewMockFplAPI(ctrl)
+	teamsParser := helpers.NewMockTeamsParser(ctrl)
+	renderer := ui.NewMockRenderer(ctrl)
+	gameweekParser := helpers.NewMockGameweekParser(ctrl)
+
+	teamsParser.EXPECT().
+		GetTeamID(gomock.Any(), gomock.Any()).
+		Return(1, nil)
+
+	fpl.EXPECT().
+		GetBootstrapData().
+		Return(responses.BootstrapData{}, nil)
+
+	gameweekParser.EXPECT().
+		GetCurrentGameweek(gomock.Any(), gomock.Any()).
+		Return(1, nil)
+
+	fpl.EXPECT().
+		GetGameweekPoints(gomock.Any(), gomock.Any()).
+		Return(responses.GameweekPoints{}, nil)
+
+	fpl.EXPECT().
+		GetGameweekLiveScores(gomock.Any()).
+		Return(responses.GameweekLiveScores{}, nil)
+
+	renderer.EXPECT().PrintHeader("Gameweek 1 points").Times(1)
+	renderer.EXPECT().PrintTeamPoints(gomock.Any(), gomock.Any(), gomock.Any()).Times(1)
+
+	cmd := BuildPointsCommand(fpl, reader, teamsParser, gameweekParser, renderer)
+
+	err := cmd.RunE(cmd, make([]string, 0))
+
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
 	}
 }
